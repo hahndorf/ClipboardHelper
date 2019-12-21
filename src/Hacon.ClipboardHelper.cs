e664fd33583926396679ae54fc9fdd7478f8ae1d
hahndorf <peterhahndorf@gmail.com>
2019-12-21 10:49:03 +0100

    Added C# code for assembly.

---
+++ src/Hacon.ClipboardHelper.cs
@@ -0,0 +1,20 @@
+﻿using uwp = Windows.ApplicationModel.DataTransfer;
+
+namespace Hacon.ClipboardHelper
+{
+    public static class Windows10Clipboard
+    {
+        public static void Copy(string text,bool enableHistory, bool enableRoaming)
+        {
+            uwp.DataPackage dataPackage = new uwp.DataPackage { RequestedOperation = uwp.DataPackageOperation.Copy };
+            dataPackage.SetText(text);
+            uwp.Clipboard.SetContentWithOptions(dataPackage, new uwp.ClipboardContentOptions() { IsAllowedInHistory = enableHistory, IsRoamable = enableRoaming });
+            uwp.Clipboard.Flush();
+        }
+
+        public static void Copy(string text)
+        {
+            Copy(text, false, false);
+        }
+    }
+}
